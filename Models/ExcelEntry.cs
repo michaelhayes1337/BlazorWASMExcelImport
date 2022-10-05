6936dc8b50a4cd00aa4722a7541da67853491350
michael hayes <58147374+michaelhayes1337@users.noreply.github.com>
2022-10-05 13:11:10 +0200

    WIP

---
+++ Models/ExcelEntry.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace TestExcelImport.Models
+{
+    public class ExcelEntry
+    {
+        [DisplayName("First Name")]
+        public string FirstName { get; set; } = string.Empty;
+        [DisplayName("Last Name")]
+        public string LastName { get; set; } = string.Empty;
+        [DisplayName("Age Test")]
+        public int? Age { get; set; }
+    }
+}
